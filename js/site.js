6a081c92db01a7f0dcc59e61991bd51bb83e670b
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2025-02-16 03:12:01 +0100

    Add language dropdown

--- js/site.js
+++ js/site.js
@@ -1,4 +1,24 @@
 "use strict";
+
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("judge0-year").innerText = new Date().getFullYear();
+});
+
+document.body.addEventListener("click", function (event) {
+    const dropdown = event.target.closest(".judge0-dropdown");
+    const dropdownBtn = event.target.closest(".judge0-dropdown-btn");
+
+    if (event.target && dropdownBtn && dropdownBtn.contains(event.target)) {
+        dropdown.querySelector(".judge0-dropdown-menu").classList.toggle("hidden");
+    } else if (event.target && event.target.classList.contains("judge0-dropdown-option")) {
+        const span = dropdown.querySelector("span");
+        span.innerText = event.target.innerText;
+        dropdown.querySelector(".judge0-dropdown-menu").classList.toggle("hidden");
+    } else {
+        document.querySelectorAll(".judge0-dropdown-menu").forEach(function (dropdownMenu) {
+            if (!dropdownMenu.contains(event.target)) {
+                dropdownMenu.classList.add("hidden");
+            }
+        });
+    }
 });
